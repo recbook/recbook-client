e13e5a4d954ad89bf59e71a75e71d06578cb877d
Youngchan Je <ochanje210@gmail.com>
2017-01-03 13:43:04 +0900

    fix eslint

--- app/components/home.js
+++ app/components/home.js
@@ -49,8 +49,9 @@ const styles = {
 };
 
 const randomColorForDev = () => {
+  // eslint-disable-next-line
   return '#' + (function co(lor){ return (lor +=
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'][Math.floor(Math.random()*16)])
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'][Math.floor(Math.random() * 16)])
   && (lor.length === 6) ? lor : co(lor); })('');
 };
 
