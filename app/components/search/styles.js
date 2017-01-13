775a1281062cb5dc1f41a5f1dc342a06fa31f9c5
Dan Hyunchan Kim <hckim0625@gmail.com>
2017-01-13 13:10:20 +0900

    Add search page.

---
+++ app/components/search/styles.js
@@ -0,0 +1,48 @@
+import {
+  Dimensions,
+  StyleSheet
+} from 'react-native';
+
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent'
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#f9f9f9',
+    height: HEIGHT * 0.11
+  },
+  searchBar: {
+    height: HEIGHT * 0.068,
+    width: WIDTH * 0.85,
+    fontSize: 15,
+    paddingLeft: 20,
+    borderWidth: 3,
+    borderColor: '#ececec',
+    borderRadius: 7,
+    marginTop: 15
+  },
+  backButton: {
+    width: 20,
+    height: 20,
+    marginTop: 25,
+    marginLeft: 15,
+    marginRight: 10
+  },
+  resultContainer: {
+    height: HEIGHT * 0.13,
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ececec'
+  },
+  resultText: {
+    fontSize: 15,
+    marginLeft: WIDTH * 0.065
+  }
+});
+
+export default styles;
