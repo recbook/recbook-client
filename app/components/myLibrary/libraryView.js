33d0a7d5228e93bb0182e71b32791ad3468655e0
Youngchan Je <ochanje210@gmail.com>
2017-02-21 17:55:48 +0900

    added reset color on onPressOut

--- app/components/myLibrary/libraryView.js
+++ app/components/myLibrary/libraryView.js
@@ -37,13 +37,13 @@ export default class LibraryView extends Component {
       onPressedBookIndex: undefined,
       onPressedColumn: undefined,
       initialBookColor: [],
-      fadeAnimRow: new Animated.Value(0),
       offset: 0,
       direction: undefined
     };
     for (let i = 0; i < this.state.dataSource.length; i = i + 1) {
       this.state.initialBookColor.push(generateRandomColor());
     }
+    this.resetBookBackgroundColor = this.resetBookBackgroundColor.bind(this);
   }
 
   static propTypes = {
@@ -52,44 +52,26 @@ export default class LibraryView extends Component {
     prevScene: PropTypes.string
   };
 
-  animateFadeRow() {
-    Animated.sequence([
-      Animated.timing(
-        this.state.fadeAnimRow,
-        {
-          toValue: 0,
-          duration: 0
-        }
-      ),
-      Animated.timing(
-        this.state.fadeAnimRow,
-        {
-          toValue: 1,
-          duration: 300
-        }
-      )
-    ]).start();
-  }
-
   handleOnPressBookStyle(index, col) {
     // todo: implement this.
     this.setState({
       onPressedBookIndex: index,
       onPressedColumn: col
     });
-    this.animateFadeRow();
   }
 
   handleOnPressBookTransition(bookInfo) {
     // todo: implement scene transition on book press here
     Actions.detailView({prevScene: this.props.prevScene, bookInfo: bookInfo.node});
     // this changing the color back to white after transition animation is done
-    setTimeout(()=>{
-      this.setState({
-        onPressedBookIndex: undefined,
-        onPressedColumn: undefined
-      })
-    }, 800)
+    setTimeout(()=>{this.resetBookBackgroundColor()}, 800);
+  }
+
+  resetBookBackgroundColor() {
+    this.setState({
+      onPressedBookIndex: undefined,
+      onPressedColumn: undefined
+    });
   }
 
   renderRow(content, index, col) {
@@ -109,6 +91,7 @@ export default class LibraryView extends Component {
           style={Styles.row}
           onPressIn={() => this.handleOnPressBookStyle(index, col)}
           onPress={() => this.handleOnPressBookTransition(content.data)}
+          onPressOut={this.resetBookBackgroundColor}
           activeOpacity={1}
         >
           {(node.thumbnail) ?
